3e6d1ec260a1e4ea3a203ee5a08662dad0de4f51
pintamonas4575 <alejandro.embi@gmail.com>
2024-11-11 21:45:35 +0100

    primer avance, visualizar boletos random

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # RL-model-The-Simpsons
 Implementation of a Reinforcement Learning (RL) model to learn to scratch the less possible surface on the scratch game of the Badulaque of the app "the Simpsons Springfield".
+
+# Flujo a seguir
+1. generar boletos random en cada ejecución
+2. generar boletos con la capa de rascar
+3. rascar de forma automática
+4. obtener porcentaje de superficie rascada 
+5. modelo de RL para rascar (lo difícil, el tema de las recompensas)
+
+# Objetivo del modelo
+Obtener la menor cantidad de superficie rascada para desvelar los premios. No se juega con las recompensas.
+
+**NOTA:** Los símbolos del boleto cambian con cada boleto.
+
+# Resursos utilizados
+ChatGPT
+
+Github emojis: [ikatyang](https://github.com/ikatyang/emoji-cheat-sheet?tab=readme-ov-file#tool)
+
+Emojipedia: [Emojipedia](https://emojipedia.org/)
+
+
+# ⚖️ Licencia 
+
+El gobierno sí que nos rasca los bolsillos.
+
+# 👤 Contacto
+
+Cualquier duda o sugerencia contactar con el autor:
+
+Alejandro Mendoza: alejandro.embi@gmail.com 
---
+++ prueba1.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+import random
+from tkinter import ttk
+from PIL import ImageGrab
+
+def capturar_y_guardar():
+    try:
+        # Obtener las coordenadas de la ventana
+        x = window.winfo_rootx()
+        y = window.winfo_rooty()
+        width = window.winfo_width()
+        height = window.winfo_height()
+
+        # Capturar la ventana
+        imagen = ImageGrab.grab(bbox=(x, y, x + width, y + height))
+        imagen.save("captura_ventana.png")  # Guardar la imagen
+        print("Captura guardada como 'captura_ventana.png'")
+        window.quit()  # Cerrar la ventana (opcional)
+    except Exception as e:
+        print("Error al capturar la ventana:", e)
+
+# symbols = ["🔨", "🪓", "💣", "🍩", "⭐"]
+symbols = ["hammer.png", "axe.png", "bomb.png", "doughnut.png", "star.png"]
+emojis = random.choices(symbols, k=3)
+
+try:
+    window = tk.Tk()
+    # window.config(bg='black')
+    window.title("Modelo RL Rasca y gana")
+    window.geometry('1000x500')
+
+    # mantener "vivas" las imágenes
+    images = []
+
+    for i, emoji in enumerate(emojis):
+        emoji_image = tk.PhotoImage(name=emoji, file=f"emojis/{emoji}")
+        emoji_image = emoji_image.subsample(1, 1)
+        images.append(emoji_image)  # Guardar referencia de la imagen
+        label = ttk.Label(window, image=emoji_image)
+        label.place(relx=0.3 + i * 0.2, rely=0.5, anchor="center")
+
+    window.after(500, capturar_y_guardar)  # Esperar 100ms antes de capturar
+    window.mainloop()
+
+except Exception as e:
+    print(e)
